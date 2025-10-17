aa864fc3483491d4ca52cbfe18359264ed93ed35
Ayaka-Nakajima <ayakanakajima20000918@gmail.com>
2025-10-17 14:44:59 +0900

    adding bootstrap and using laravel to reduce repetetive codes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/a', function () {
+    return view('contact');
 });
+
