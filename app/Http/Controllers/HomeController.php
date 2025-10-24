4581299b0503c128ebaa80755daf8274724e2b81
Ayaka-Nakajima <ayakanakajima20000918@gmail.com>
2025-10-24 15:04:23 +0900

    カテゴリごとのポスト表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,19 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index()
     {
         //方法３：Eloquent ORMを使ってcategoriesテーブルから全てのカテゴリを取得
-        $allCategories = Category::all();
+        $categories = Category::all();
 
         //方法２：ここでDBファサードを使ってcategoriesテーブルから全てのカテゴリを取得
         //$allCategories = DB::table('categories')->get();
         
         //方法１：hardcoded categories for demonstration; in a real app, these might come from a database
         //$allCategories = ['category1', 'category2', 'category3'];
-        return view('home', ['categories' => $allCategories]);
+        
+        $cid = request('category_id');
+
+        //全ての投稿を取得する場合のコード例
+        // $posts = Post::latest()->get();
+
+        //whenメソッドを使って、category_idパラメータが存在する場合にのみフィルタリングを適用
+        //if文を使わずにクエリビルダをチェーンする形で条件付きクエリを実現できます
+        $posts = Post::when($cid, function ($query) use ($cid) {
+            return $query->where('category_id', $cid);
+        })->latest()->get();
+
+        //test code to verify Post model is working
+        //$posts = Post::orderby('id', 'desc')->get();
+        // print_r($posts);
+        // die();
+
+        //デバッグ用：リクエストパラメータの確認
+        // ?=10のようにURLにパラメータを追加してアクセスすると、値が取得できます
+        //die("the request id is " . request('category_id'));
+
+        //ビューにデータを渡す方法２（compact関数を使う）
+        return view('home',compact('posts', 'categories'));
+
+        
+        //ビューにデータを渡す方法１
+        // return view('home', 
+        // [
+        //     'categories' => $allCategories,
+        //     'posts' => $posts
+        // ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,89 +1,33 @@
-@extends('layout.app') 
+@extends('layout.app')
 
 @section('content')
+        <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Welcome to Blog Home!</h1>
+                    <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                </div>
+            </div>
+        </header>
+
+
 <!-- Page content-->
 <div class="container">
-    <header class="py-5 bg-light border-bottom mb-4">
-    <div class="container">
-        <div class="text-center my-5">
-            <h1 class="fw-bolder">Hello!</h1>
-            <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
-        </div>
-    </div>
-    </header>
     <div class="row">
         <!-- Blog entries-->
         <div class="col-lg-8">
-            <!-- Featured blog post-->
+            @foreach ($posts as $post)
             <div class="card mb-4">
-                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
                 <div class="card-body">
-                    <div class="small text-muted">January 1, 2023</div>
-                    <h2 class="card-title">Featured Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                    <!--仮に$postsが単一の投稿オブジェクトであると仮定しています-->
+                    <h2 class="card-title h4">{{ $post->title }}</h2>
+                    <p class="card-text">{{ Str::limit($post->text, 80) }}</p>
                     <a class="btn btn-primary" href="#!">Read more →</a>
                 </div>
             </div>
-            <!-- Nested row for non-featured blog posts-->
-            <div class="row">
-                <div class="col-lg-6">
-                    <!-- Blog post-->
-                    <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">January 1, 2023</div>
-                            <h2 class="card-title h4">Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
-                        </div>
-                    </div>
-                    <!-- Blog post-->
-                    <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">January 1, 2023</div>
-                            <h2 class="card-title h4">Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <!-- Blog post-->
-                    <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">January 1, 2023</div>
-                            <h2 class="card-title h4">Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
-                        </div>
-                    </div>
-                    <!-- Blog post-->
-                    <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">January 1, 2023</div>
-                            <h2 class="card-title h4">Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam.</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Pagination-->
-            <nav aria-label="Pagination">
-                <hr class="my-0" />
-                <ul class="pagination justify-content-center my-4">
-                    <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
-                    <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">3</a></li>
-                    <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">15</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">Older</a></li>
-                </ul>
-            </nav>
+            @endforeach
         </div>
         <!-- Side widgets-->
         <div class="col-lg-4">
@@ -104,12 +48,16 @@
                     <div class="row">
                         <div class="col-sm-6">
                             <ul class="list-unstyled mb-0">
-                                @foreach($categories as $category)
-                                    {{-- nameを指定して取得するように変更したので、$category->nameでアクセス --}}
-                                    <li><a href="#!">{{ $category->name }}</a></li>
-                                @endforeach
-                                {{-- もともとあったものはこれ。動的に入れるため、削除
-                                <li><a href="#!">Web Design</a></li>
+
+
+                                @foreach ($categories as $category)
+
+                                <li><a href="{{route('home')}}?category_id={{ $category->id }}">{{ $category->name }}</a></li>
+                                    
+                                @endforeach 
+
+
+                                {{-- <li><a href="#!">Web Design</a></li>
                                 <li><a href="#!">HTML</a></li>
                                 <li><a href="#!">Freebies</a></li> --}}
                             </ul>
@@ -134,3 +82,4 @@
 </div>
 
 @endsection
+
